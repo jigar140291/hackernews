c2e31ceb31e5ba57a8a20bd62d1a58ef8754b6e4
jigar140291 <jigar.gala140291@gmail.com>
2021-04-05 19:47:33 +0530

    Updated pagination text

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -16,9 +16,11 @@ export class NewsComponent {
     "top-stories":"top",
     "best-stories":"best"
   }
+  public storyType: IStory["type"];
+
   constructor(public newsService: NewsService, private activeRoute: ActivatedRoute) {
-    let storyType = this.storyMapping[this.activeRoute.snapshot.url.join('')];
-    this.loadNews(1, storyType);
+    this.storyType = this.storyMapping[this.activeRoute.snapshot.url.join('')];
+    this.loadNews(1, this.storyType);
   }
 
   public loadNews(pageNumber: number, storyType: IStory["type"]){
