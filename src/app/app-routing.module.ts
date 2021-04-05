015349a2c38da7b14165d5a83db6fa5d9d61548d
jigar140291 <jigar.gala140291@gmail.com>
2021-04-05 14:37:10 +0530

    Introducing routing module with new-stories, login and register route.
    - login/signup will share same component with configuration

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NewsComponent } from './news/news.component';
+import { LoginComponent } from './user/login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/new-stories', pathMatch: 'full' },
+  { path: 'new-stories', component: NewsComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: LoginComponent}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NewsModule } from './news/news.module';
+import { AppRoutingModule } from './app-routing.module';
+import { UserModule } from './user/user.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, NewsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, NewsModule, UserModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
